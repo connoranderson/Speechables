9fd75fdb9d888c4167ed6f54b48c7f217298b52c
evancchow <evan@ubuntu.(none)>
2015-01-16 22:50:55 -0800

    substituted avconv and it worked

--- processing/processing.sh
+++ processing/processing.sh
@@ -4,7 +4,7 @@ rm -r download/videos/*
 FILENAME='video.mp4'
 cd processing/videos
 
-ffmpeg -i $FILENAME -ss 00:01:30 -t 00:00:30 -c copy ./processed/output1.mp4 
+avconv -i $FILENAME -ss 00:01:30 -t 00:00:30 -c copy ./processed/output1.mp4 
 rm $FILENAME
 cd ..
 cd ..
