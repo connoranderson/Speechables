469c7461c871e4205b2b67b5ccda468696607e51
Connor Anderson <connora@stanford.edu>
2015-01-17 02:21:17 -0500

    Download audio and convert to .wav for audio processing

--- processing/processing.sh
+++ processing/processing.sh
@@ -1,7 +1,18 @@
 cp -r download/videos/video.mp4 processing/videos/video.mp4
 rm -r download/videos/*
+cp -r download/audio/audio.ogg audio/audio.ogg
+rm -r download/audio/*
 
 FILENAME='video.mp4'
+AUDIONAME='audio.ogg'
+
+cd audio
+
+ffmpeg -i $AUDIONAME audio.wav
+rm $AUDIONAME
+
+cd ..
+
 cd processing/videos
 
 ffmpeg -i $FILENAME -ss 00:01:30 -t 00:00:30 -c copy ./processed/output1.mp4 
