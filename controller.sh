9c6414f19e48481112cf9fab2d403f07a460e6e3
evancchow <evan@ubuntu.(none)>
2015-01-17 10:11:24 -0800

    before modifying directories

--- controller.sh
+++ controller.sh
@@ -11,6 +11,8 @@ cd srt/
 echo 'Acquiring Subtitles'
 python downloadsrt.py
 cd ..
-mv srt/content.srt audio/content.srt
-echo 'Processing Video'
-./processing/processing.sh
+mv srt/content.srt audio/audio.srt
+echo 'Converting audio into chunks'
+audio/resample.sh
+# echo 'Processing Video'
+# ./processing/processing.sh
