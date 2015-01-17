3b91475f1c095fc78faa7b0ed5a3fce0498af10c
evancchow <evan@ubuntu.(none)>
2015-01-17 14:02:39 -0800

    yay

--- processing/processing.sh
+++ processing/processing.sh
@@ -1,32 +1,31 @@
-cp -r download/videos/video.mp4 processing/videos/video.mp4
-rm -r download/videos/*
-cp -r download/audio/audio.ogg audio/audio.ogg
-rm -r download/audio/*
+# cp -r download/videos/video.mp4 processing/videos/video.mp4
+# rm -r download/videos/*
+# cp -r download/audio/audio.ogg audio/audio.ogg
+# rm -r download/audio/*
+
+cp -r ../download/videos/video.mp4 ./videos/video.mp4
+# rm -r download/videos/*
 
 FILENAME='video.mp4'
-AUDIONAME='audio.ogg'
 
-cd audio
+cd ./videos
+# avconv -framerate $FILENAME
 
-ffmpeg -i $AUDIONAME audio.wav
-rm $AUDIONAME
+while read line
+do
+    stringarray=($line)
+    NAME=${stringarray[0]}'.mp4'
+    echo $NAME
+    TIMESTAMP1=${stringarray[1]}
+    echo $TIMESTAMP1
+    TIMESTAMP2=${stringarray[2]}
+    echo $TIMESTAMP2
+    avconv -i $FILENAME -ss $TIMESTAMP1 -t $TIMESTAMP2 -c copy ./processed/$NAME    
 
-cd ..
+done < "$1"
 
-cd processing/videos
-
-# while read line
-# do
-#     lineText=$line
-#     stringarray($lineText)
-#     NAME = stringarray[0] + '.mp4'
-#     echo $NAME
-#     TIMESTAMP = stringarray[1]
-#     ffmpeg -i $FILENAME -ss 00:00:05 -t 00:00:05 -c copy ./processed/$NAME    
-# done < $1
-
-ffmpeg -i $FILENAME -ss 00:00:05 -t 00:00:05 -c copy ./processed/output1.mp4 
-rm $FILENAME
+#ffmpeg -i $FILENAME -ss 00:00:05 -t 00:00:05 -c copy ./processed/output1.mp4 
+#rm $FILENAME
 cd ..
 cd ..
 
