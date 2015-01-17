bf9df4e306137ae43a4adbdd29fb5b402ee6238a
Connor Anderson <connora@stanford.edu>
2015-01-16 19:56:18 -0800

    Added ffmpeg library, updated processing

--- download/download.py
+++ download/download.py
@@ -1,8 +1,7 @@
 import pafy
+import os.path
 
-url = "https://www.youtube.com/watch?v=8gXBkOYZYnk"
+url = "https://www.youtube.com/watch?v=Nbol8oMNggs"
 video = pafy.new(url)
-
 best = video.getbest(preftype="mp4")
-
-filename = best.download(filepath="../videos/", quiet=False)
+filename = best.download(filepath="../download/videos/")
