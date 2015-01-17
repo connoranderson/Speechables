a674edcafe55a6cdb6b9c55fdc6801356cd072e7
Connor Anderson <connora@stanford.edu>
2015-01-16 18:50:55 -0800

    Awesome

--- download/download.py
+++ download/download.py
@@ -1,5 +1,8 @@
 import pafy
 
-url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
+url = "https://www.youtube.com/watch?v=8gXBkOYZYnk"
 video = pafy.new(url)
 
+best = video.getbest(preftype="mp4")
+
+filename = best.download(filepath="../videos/", quiet=False)
