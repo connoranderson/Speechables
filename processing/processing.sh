b15902437ec58000a734cc374c9a92cc5971feb4
evancchow <evan@ubuntu.(none)>
2015-01-17 20:05:26 -0800

    jpgs works, just need to add video

--- processing/processing.sh
+++ processing/processing.sh
@@ -3,6 +3,9 @@
 # cp -r download/audio/audio.ogg audio/audio.ogg
 # rm -r download/audio/*
 
+pwd
+# rm ./videos/processed/*
+
 cp -r ../download/videos/video.mp4 ./videos/video.mp4
 # rm -r download/videos/*
 
@@ -14,13 +17,54 @@ cd ./videos
 while read line
 do
     stringarray=($line)
+    ABBR=${stringarray[0]}
     NAME=${stringarray[0]}'.mp4'
-    echo $NAME
+    # echo $NAME
     TIMESTAMP1=${stringarray[1]}
-    echo $TIMESTAMP1
+    # echo $TIMESTAMP1
     TIMESTAMP2=${stringarray[2]}
-    echo $TIMESTAMP2
-    avconv -i $FILENAME -ss $TIMESTAMP1 -t $TIMESTAMP2 -c copy ./processed/$NAME    
+    # echo $TIMESTAMP2
+    ISLONGWORD=${stringarray[3]}
+
+    # Continue if you see the file already
+    if [[ -f ./processed/$NAME ]]
+    then
+        echo "Already saw $ABBR"
+        continue
+    fi
+
+    # If statement.
+    if [[ $ISLONGWORD -eq '1' ]] # test if long word (has vid)
+    then
+        echo "Video for $ABBR"
+    	avconv -i $FILENAME -ss $TIMESTAMP1 -t $TIMESTAMP2 -c copy ./processed/$NAME    
+    else
+        echo "Non-audio video for short $ABBR"
+        # echo "Sorry no vid for $NAME"
+    	# Create jpgs of that clip
+    	avconv -i $FILENAME -ss $TIMESTAMP1 -t $TIMESTAMP2 ./processed/$ABBR-%d.jpg
+
+        # Create video of clip
+        avconv -i ./processed/$ABBR-%d.jpg ./processed/$NAME
+
+        # rm ./processed/*.jpg
+        # ffmpeg -loop 1 -i img.jpg -i audio.wav -c:v libx264 -c:a aac -strict experimental -b:a 192k -shortest out.mp4
+
+     #    # get audio
+     #    avconv -i $FILENAME -ss $TIMESTAMP1 -t $TIMESTAMP2 temp.wav
+
+    	# # Combine back into a video with sound!
+     #    avconv -loop -i $NAME-%d.jpg -i temp.wav -c:v libx264 -c:a copy -shortest ./processed/$NAME
+
+     #    rm $NAME*.jpg
+     #    rm temp.wav
+    fi
+    # # Make a video.
+    # avconv -i $FILENAME -t $TIMESTAMP2 -ss $TIMESTAMP1 image-%d.jpg
+    
+    # ffmpeg -f image2 -i Path/To/File/filename%3d.jpg test.avi
+    # avconv -pattern_type glob -i '*.jpg' -c:v libx264 $NAME.mp4
+    # rm *.jpg
 
 done < "$1"
 
